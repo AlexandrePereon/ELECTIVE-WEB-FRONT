d208bdcf3360de54aca9f3595003091e0de86309
Mallory Gesbert <mallorygesbert@gmail.com>
2024-03-26 10:19:13 +0100

    Modification du breadcrumb pour qu'il soit dynamic

--- src/components/Breadcrumb/breadcrumb.js
+++ src/components/Breadcrumb/breadcrumb.js
@@ -1,12 +1,31 @@
 import React from "react";
 
 const Breadcrumb = () => {
+    const splittedUrl = window.location.href.split("/");
+    splittedUrl.splice(0, 3);
+
+    const getHrefOfThePart = (index) => {
+        return(
+            splittedUrl.slice(0,index+1).join("/")
+        );    
+    } 
+
+    const partUrlList = splittedUrl.map((part,index) => {
+        if(index == splittedUrl.length-1){
+            return(
+                <li key={index}> <p>{part} </p></li>
+            );
+        } else {
+            return(
+                <li key={index}> <a href={"/" + getHrefOfThePart(index)}>{part} </a></li>
+            );
+        }
+    })
+
     return ( 
         <div className="text-sm breadcrumbs">
             <ul>
-                <li><a>Home</a></li> 
-                <li><a>Documents</a></li> 
-                <li>Add Document</li>
+                {partUrlList}
             </ul>
         </div>
     );
