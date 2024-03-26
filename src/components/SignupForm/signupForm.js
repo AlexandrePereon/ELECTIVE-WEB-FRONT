698e906a440a8eab324c996bf94af26fd7adeb3e
Mallory Gesbert <mallorygesbert@gmail.com>
2024-03-26 13:36:28 +0100

    Suppression du password en trop dans la création de compte

--- src/components/SignupForm/signupForm.js
+++ src/components/SignupForm/signupForm.js
@@ -92,20 +92,6 @@ const SignupForm = () => {
                         />
                     </div>
                 </div>
-                <div className="sm:col-span-4">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 ">
-                        Password
-                    </label>
-                    <div className="mt-2">
-                        <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="password"
-                        className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
                 <div className="sm:col-span-3">
                 <Password handlesetIsPasswordValid={handlesetIsPasswordValid}/>
                 <label htmlFor="role" className="block text-sm font-medium leading-6 ">
