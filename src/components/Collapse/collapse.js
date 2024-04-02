606e937d838b337ea89fa18a53a619cf0b56f423
yanis <yanis@DESKTOP-2TCBKT2>
2024-04-02 12:53:34 +0200

    feature/add-pagination-collapse

--- src/components/Collapse/collapse.js
+++ src/components/Collapse/collapse.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import BrandCard from "../BrandCard/brandCard";
 import AddNumberButton from "../AddNumberButton/addNumberButton";
 import Loader from "../Loader/loader";
+import Pagination from "../Pagination/pagination";
 import useGetAllArticlesFromRestaurant from "../../hooks/data/get/useGetAllArticlesFromRestaurant";
 
 
 const Collapse = ({title, handleOnChange, submissionTunnelFormListArticle, restaurantId}) => {
-    const {articlesData, isLoadingArticles} = useGetAllArticlesFromRestaurant(restaurantId,1);
+    const [paginationArticles, setPaginationArticles]=useState(1)
+
+    const handleSetPagination = (value, type) => {
+     if((value>0) && (value<=maxPageArticles)){
+        type === "articles" &&  setPaginationArticles(value)
+            
+         }
+    }
+    
+    const {articlesData, isLoadingArticles, maxPageArticles} = useGetAllArticlesFromRestaurant(restaurantId,paginationArticles);
     return(
         <div className="collapse collapse-plus rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default sm:px-7.5 xl:pb-1">
             <input type="checkbox" /> 
@@ -27,8 +37,13 @@ const Collapse = ({title, handleOnChange, submissionTunnelFormListArticle, resta
                     >
                         <AddNumberButton handleOnChange={handleOnChange} value={article._id} numberOfArticle={submissionTunnelFormListArticle.filter(id => id === article._id).length}/>
                     </BrandCard>
-                ))
-        }
+                ))}
+                <Pagination 
+                    pagination={paginationArticles} 
+                    handleSetPagination={handleSetPagination}
+                    maxPagination={maxPageArticles}
+                    type={"articles"}
+                />
            
             </div>
         </div>
