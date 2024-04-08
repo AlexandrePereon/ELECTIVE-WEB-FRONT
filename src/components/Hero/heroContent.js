5c1998d0c685734e7ebff75e3cb429a1dda0a85f
Mallory Gesbert <mallorygesbert@gmail.com>
2024-04-08 11:26:17 +0200

    Maj heroContent

---
+++ src/components/Hero/heroContent.js
@@ -0,0 +1,35 @@
+import ImageHero from "../../assets/images/photoHero.jpg"
+
+const heroContent = [
+    {
+        role : "user",
+        description : "Bienvenue sur l'espace client",
+        image : ImageHero
+      },
+      {
+        role : "restaurant",
+        description : "Bienvenue sur l'espace restaurateur",
+        image : ImageHero
+      },
+      {
+        role : "deleveryman",
+        description : "Bienvenue sur l'espace livreur",
+        image : ImageHero
+      },
+      {
+        role : "marketing",
+        description : "Bienvenue sur l'espace commercial",
+        image : ImageHero
+      },
+      {
+        role : "technical",
+        description : "Bienvenue sur l'espace technique",
+        image : ImageHero
+      },
+      {
+        role : "developer",
+        description : "Bienvenue sur l'espace developpeur",
+        image : ImageHero
+      }];
+
+export default heroContent
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,16 +3,17 @@ import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
 import Hero from "../components/Hero/hero";
 import useAuthentication from "../hooks/useAuthentication";
+import heroContent from "../components/Hero/heroContent";
 
 
 const HomePage = () => {
     const {getUserInfosFromSessionStorage}=useAuthentication();
     const userInfos = getUserInfosFromSessionStorage();
-
+    console.log(heroContent.find((item)=> item.role === userInfos?.role))
     return (
         <Fragment>
             <Header role={userInfos?.role}/>
-            <Hero name={userInfos?.firstName}/>
+            <Hero name={userInfos?.firstName} heroContent={heroContent.find((item)=> item.role === userInfos?.role)} />
             <Footer/>
         </Fragment>
     )
