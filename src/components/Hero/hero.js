f06183c5fadb73c94d9e293157e4d5b401dae253
Mallory Gesbert <mallorygesbert@gmail.com>
2024-04-08 13:58:30 +0200

    Fix Hero

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,18 +1,35 @@
 import React from "react";
 import TitleFade from "../TitleFade/titleFade";
+import ImageHero from "../../assets/images/photoHero.jpg"
 
 const Hero = ({name, heroContent}) => {
-    return(
-        <div className="hero min-h-screen bg-light-grey">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={heroContent.image} alt="" className="sm:max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                <TitleFade title={"Bienvenue " + (name ? name : "sur CESI Eats")}/>
-                <p className="py-6">{heroContent.description}</p>
+
+    if(heroContent) {
+        return(
+            <div className="hero min-h-screen bg-light-grey">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={heroContent?.image} alt="" className="sm:max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                    <TitleFade title={"Bienvenue " + (name ? name : "sur CESI Eats")}/>
+                    <p className="py-6">{heroContent?.description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return(
+            <div className="hero min-h-screen bg-light-grey">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={ImageHero} alt="" className="sm:max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                    <TitleFade title={"Bienvenue " + (name ? name : "sur CESI Eats")}/>
+                    <p className="py-6">{"Bienvenue sur le site de CESI Eats"}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
 }
 
 export default Hero;
