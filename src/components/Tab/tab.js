767873ff2751998b21aafd2c876d3385f948135b
Mallory Gesbert <mallorygesbert@gmail.com>
2024-04-10 10:07:24 +0200

    responsive tab

--- src/components/Tab/tab.js
+++ src/components/Tab/tab.js
@@ -3,7 +3,7 @@ import React from "react";
 const Tab = ({steps, partsName, handleOnSwitchSteps}) => {
 
     const tabList = partsName.map((tab, index)=> {
-    return(<button type="button" className={`content-center p-3 tab w-max ${steps === index && 'bg-medium-green text-black'}`} onClick={()=>{handleOnSwitchSteps(index)}} key={index}>{tab}</button>)
+    return(<button type="button" className={`content-center p-auto tab m-auto w-fit h-fit leading-5 ${steps === index && 'bg-medium-green text-black'}`} onClick={()=>{handleOnSwitchSteps(index)}} key={index}>{tab}</button>)
 })
     return(
         <div role="tablist" className="tabs tabs-boxed w-fit m-auto mt-2.5">
